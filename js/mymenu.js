663d5c8407520547f03cc8b70e2b664734608ad9
AanNahl Software <aannahlsoftware@AanNahls-MacBook-Pro.local>
2019-04-15 16:08:36 +0600

    readme

---
+++ js/mymenu.js
@@ -0,0 +1,9 @@
+(function ($) {
+    "use strict";
+    var $ = jQuery;
+    $('header nav').meanmenu({
+        meanScreenWidth: "1200",
+        meanRevealPosition: "right",
+    });
+
+})(jQuery);	
