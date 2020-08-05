86f06687f9206b44b5635ee96f9a666821321913
jamiul.alam <jamiul.alam@one-proof.com>
2020-08-05 13:39:35 +0600

    add profile image

--- js/mymenu.js
+++ js/mymenu.js
@@ -2,8 +2,8 @@
     "use strict";
     var $ = jQuery;
     $('header nav').meanmenu({
-        meanScreenWidth: "1200",
-        meanRevealPosition: "right",
+        meanScreenWidth: "2400",
+        meanRevealPosition: "left",
     });
 
 })(jQuery);	
